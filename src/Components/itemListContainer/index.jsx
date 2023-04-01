c741bbb3b7b040adcad0ac5554eda87b71b48789
matiasjavier1985 <matiasjavier1985@gmail.com>
2023-04-01 17:25:56 -0300

    UPD0104231730PM

--- src/Components/itemListContainer/index.jsx
+++ src/Components/itemListContainer/index.jsx
@@ -4,22 +4,27 @@ import ItemList from "../itemList";
 function Contenedor({marca,isRoute}) {
     const [bike,setBike]= useState([])
 
-   useEffect(()=>{
-    fetch("/mocks/bikes.json")
-    .then((res)=> res.json())
-    .then ((datos)=>{
-        
-        setTimeout(() => {
-            
-            if (isRoute){
-                const bikefilter = datos.filter((prod)=>{
-                    console.log(prod.brand == marca) 
-                    return prod.brand == marca;})
+    useEffect(()=>{
+        const db = getFirestore()
+        const itemsCollection = collection(db, 'items')
 
-                setBike(bikefilter)
-            }else{setBike(datos)}
-        },2000);
-    })
+        if (isRoute) {
+            const bike = query(itemsCollection, where('marca', '==' , marca))
+            
+            getDocs(bike)
+            .then((snapshot)=>{
+                const docs = snapshot.docs
+                setProducts(docs.map((doc)=>({id:doc.id, ...doc.data()} )))
+
+            }).catch((error)=>console.log(error))
+        }
+        else{
+            getDocs(itemsCollection)
+            .then((snapshot)=>{
+                const docs = snapshot.docs
+                    setProducts(docs.map((doc)=>({id:doc.id, ...doc.data()} )))
+            }).catch((error)=>console.log(error))
+        }
    },[marca])
 
     return(
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,30 @@ import Root from './Components/routes/root'
 import "./index.css"
 import ItemRoot from "./Components/routes/item";
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCNfZk0yblzPQR1IogSDZrKWT8eAIqEIEY",
+  authDomain: "bikeshop-95f40.firebaseapp.com",
+  projectId: "bikeshop-95f40",
+  storageBucket: "bikeshop-95f40.appspot.com",
+  messagingSenderId: "141154442154",
+  appId: "1:141154442154:web:d458c6ee8dfb31207569d3",
+  measurementId: "G-CQCSVF0NS1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 const router = createBrowserRouter([
   {
     path: "/",
